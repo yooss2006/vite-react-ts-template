383ac759cbc2b8745f9458a54a7b4795e4e1e88e
yooss2006 <yooss2006@naver.com>
2024-10-14 14:43:45 +0900

    Feat: vite.config.ts 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteTsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  optimizeDeps: { exclude: ["fsevents"] },
+});
