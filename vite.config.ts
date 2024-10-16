3468c049def994f154ce4aea815480a4bb800ee8
yooss2006 <yooss2006@naver.com>
2024-10-16 13:58:02 +0900

    Fix: package.json scripts 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import viteTsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
   preview: {
     port: 3000,
   },
-  optimizeDeps: { exclude: ["fsevents"] },
+  optimizeDeps: { exclude: ['fsevents'] },
 });
